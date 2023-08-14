8208a03f0471e509d18da5c2265468cad4923bca
Maximilian Noppel <maximilian.noppel@kit.edu>
2023-08-14 17:58:25 +0200

    Times and Dates and deadlineapi.json export

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import flask
 import pathlib
 import os
+import json
 
+from datetime import datetime
 import backend
 
 app = flask.Flask(__name__, static_url_path='/static')
@@ -10,10 +12,37 @@ app = flask.Flask(__name__, static_url_path='/static')
 class EntryNotFoundException(Exception):
     pass
 
+@app.template_filter('datefilter')
+def _jinja2_filter_date(datestr, fmt=None):
+    try:
+        date = datetime.strptime(datestr,"%Y-%m-%d")
+        return date.strftime('%d. %b %Y')
+    except:
+        return f"date unparsable: {datestr}"
+
+@app.template_filter('datetimefilter')
+def _jinja2_filter_date(datestr, fmt=None):
+    try:
+        date = datetime.strptime(datestr,"%Y-%m-%d %H:%M:%S %z")
+        return date.strftime('%d. %b %Y %H:%M:%S %z')
+    except:
+        return f"datetime unparsable: {datestr}"
 
 @app.route("/", endpoint='indexhtml')
 def indexhtml():
     return app.send_static_file('index.html')
+
+@app.route("/deadlineapi.json", endpoint='deadlineapijson')
+def deadlineapijson():
+    conn = backend.get_db()
+    rows = backend.get_entries(conn)
+    conn.close()
+
+    templatefile = open(os.path.join('static','deadlineapi-template.json'),'r')
+    template = json.load(templatefile)
+
+    template['deadlines'] = [dict(ix) for ix in rows]
+    return flask.jsonify(template)
 
 @app.route("/<path:path>", endpoint='index')
 def index(path):
@@ -24,7 +53,6 @@ def index(path):
 def editor():
     conn = backend.get_db()
     entries = backend.get_entries(conn)
-    print(entries)
     return flask.render_template('editor.html',entries=entries)
 
 @app.route("/editor/edit/<int:id>",endpoint='editor_edit_entry', methods=['GET'])
@@ -69,7 +97,6 @@ def editor_edit_entry_save(id):
     backend.delete_entry(conn, id)
     
     return flask.render_template('editor_deleted_entry.html',entry=entry)
-
 
 if __name__ == "__main__":
     app.run()
